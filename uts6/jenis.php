6dd58e6ac9d91aec568047620f890bd878665899
WilliamWijaya217201 <90763124+WilliamWijaya217201@users.noreply.github.com>
2021-11-09 22:36:20 +0700

    Update jenis.php
    menambahkan jenis jenis pasta, edit made by, link

--- uts6/jenis.php
+++ uts6/jenis.php
@@ -40,6 +40,47 @@ Nama (NRP)
 </header>
 
 <!-- JENIS PASTA -->
+            <div class="img1">
+            <div class="text">
+            </div>
+        </div>
+            <article class="padding">
+                <p><b>Pasta</b> adalah makanan olahan yang digunakan pada masakan Italia, yang biasanya terbuat dari adonan tidak beragi berbahan tepung gandum durum (semolina) yang dicampur dengan air atau telur dan dibentuk menjadi lembaran-lembaran atau bentuk-bentuk yang beragam, yang kemudian dimasak dengan cara direbus atau dipanggang. Di Indonesia, jenis pasta yang populer adalah spageti, makaroni dan lasagna.</p> 
+                <br>
+                <p>Berikut beberapa jenis-jenis pasta :</p>
+                <table>
+                    <li>Spaghetti : pasta dengan helai panjang dan sangat halus</li>
+                    <img src="images/spaghetti.jpg" width="200px" height="100px">
+                    <li>Linguine : pasta yang mirip dengan spaghetti tetapi lebih lebar dibandingkan dengan potongan spaghetti</li>
+                    <img src="images/linguine.jpg" width="200px" height="150px">
+                    <li>Fettuccine : pasta yang memiliki bentuk rata seperti pita</li>
+                    <img src="images/fettuccine.jpg" width="200px" height="125px">
+                    <li>Capellini : adonannya mirip dengan spaghetti tetapi memiliki diameter lebih kecil</li>
+                    <img src="images/capellini.jpg" width="250px" height="200px">
+                    <li>Bucatini : pasta yang mirip juga dengan spaghetti tetapi memiliki lubang di tengahnya</li>
+                    <img src="images/bucatini.jpg" width="200px" height="300px">
+                    <li>Ravioli : lembaran pasta ditumpuk membentuk kantung-kantung kecil</li>
+                    <img src="images/ravioli.jpg" width="200px" height="200px">
+                    <li>Tortellini : pasta berukuran kecil dan berbentuk seperti cincin</li>
+                    <img src="images/tortellini.jpg" width="225px" height="150px">
+                    <li>Fusilli : pasta yang memiliki bentuk seperti pembuka botol tetapi memiliki spiral lebih rapat</li>
+                    <img src="images/fusilli.jpg" width="225ps" height="150px">
+                    <li>Farfalle : pasta yang berbentuk dasi kupu-kupu</li>
+                    <img src="images/farfalle.jpg" width="250px" height="200px">
+                    <li>Conchiglie : pasta yang memiliki bentuk menyerupai kerang</li>
+                    <img src="images/conchiglie.jpg" width="200px" height="175px">
+                    <li>Gnocchi : pasta yang berbentuk seperti pangsit</li>
+                    <img src="images/gnocchi.jpg" width="275px" height="225px">
+                    <li>Penne : penne adalah jenis pasta berlubang, dinamai penne karena bentuknya yang mirip dengan pena</li>
+                    <img src="images/penne.jpg" width="225px" height="175px">
+                    <li>Ziti : pasta yang bentuknya mirip seperti penne tetapi memiliki potong lurus di bagian ujungnya</li>
+                    <img src="images/ziti.jpg" width="250px" height="200px">
+                    <li>Orecchiette : bentuk pasta yang mirip dengan telinga kecil</li>
+                    <img src="images/orecchiette.jpeg" width="200px" height="200px">
+                    <li>Lasagna : pasta yang berbentuk lembaran datar</li>
+                    <img src="images/lasagna.jpg" width="250px" height="200px">
+                </table>
+			
 <div class="container yellow grayscale-min padding-64 xlarge" id="menu">
 	<div class="content">
 		<div class="row center border border-dark-grey">
@@ -121,15 +162,15 @@ Nama (NRP)
   <div class="content">
     <h1 class="center xlarge" style="margin-bottom:64px">Credits</h1>
     <p>Sumber data yang kami dapatkan : </p>
-    <p>1. *Link*</p>
-    <p>2. *Link*</p>
-    <p>3. *Link*</p>
+    <p>1. https://www.kompas.com/food/read/2020/10/14/190700375/15-jenis-pasta-yang-sering-dijumpai-di-indonesia?page=all</p>
+    <p>2. https://www.unileverfoodsolutions.co.id/id/inspirasi-chef/knorr-world-cuisines/kenali-bentuk-pasta-anda-plus-beberapa-resep-pasta-untuk-dicoba.html</p>
+    <p></p>
   </div>
 </div>
 
 <!-- Footer -->
 <footer class="center black padding-48 large">
-  <p>Made By 1672034 | *NRP* | *NRP*</p>
+  <p>Made By 1672034 | 2172011 | 2172025</p>
   <p class="tiny">Insipired By Some Web Template From W3 School</p>
 </footer>
 
