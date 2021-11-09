ef8d55daf3b3a8ffb7d17e75126084c458b5fef3
Morel_2172025 <2172025@maranatha.ac.id>
2021-11-09 20:09:08 +0800

    Add files via upload

---
+++ UTSList/list.php
@@ -0,0 +1,199 @@
+<!--2172025 Morel Mychael C.A-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/web_style.css">
+</head>
+<body>
+<div class="w3-top">
+  <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+    <a href="#home" class="w3-bar-item w3-button">Gourmet au Catering</a>
+    <!-- Right-sided navbar links. Hide them on small screens -->
+    <div class="w3-right w3-hide-small">
+      <a href="#about" class="w3-bar-item w3-button">About</a>
+      <a href="#menu" class="w3-bar-item w3-button">Menu</a>
+      <a href="#contact" class="w3-bar-item w3-button">Contact</a>
+    </div>
+  </div>
+</div>
+
+
+<table>
+  <tr>
+    <td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/1.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+
+    <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://www.google.com/maps/place/Pasta+Kangen+Ungu+Bandung/@-6.9299435,107.6373078,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68e974001b86cb:0xb647545d6fb41022!8m2!3d-6.9299488!4d107.6394965" target="_blank">Pasta Kangen Ungu Bandung</h1><br>
+      <h5 class="w3-center">Jl. Gatot Subroto No.337, Maleer, Kec. Batununggal, Kota Bandung, Jawa Barat 40274</h5>
+    </div></a>
+  </div></td>
+</tr>
+<tr>
+  <td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/2.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+
+    <td><div class="w3-col m6 w3-padding-large"><a href="https://fashion-e-pasta-bandung.business.site/?utm_source=gmb&utm_medium=referral" target="_blank">
+      <h1 class="w3-center">Fashion E Pasta</h1><br>
+      <h5 class="w3-center">Jl. Bukit Pakar Timur No.112, Ciburial, Kec. Cimenyan, Bandung, Jawa Barat 40198</h5>
+    </div></a>
+  </div></td>
+</tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+<img src="image/3.png" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+
+    <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://www.google.com/maps/place/Osteria+Bandung/@-6.8824188,107.5772472,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68e683fce27f5f:0x71779467e2a33daf!8m2!3d-6.8824241!4d107.5794359" target="_blank">Osteria Bandung</h1><br>
+      <h5 class="w3-center">Jl. Lemah Neundeut No.9, Sukawarna, Kec. Sukajadi, Kota Bandung, Jawa Barat 40164</h5>
+    </div></a>
+  </div></td></tr>
+<tr>
+  <td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/4.jpeg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+
+    <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://food.grab.com/id/en/restaurant/warung-pasta-bandung-dago-delivery/IDGFSTI00000lcj" target="_blank">WARUNG PASTA Bandung</h1><br>
+      <h5 class="w3-center">Jl. Ganesa No.4, Lb. Siliwangi, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132</h5></a>
+    </div>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/5.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+
+    <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://www.google.com/maps/place/Pasta+Nafisa/@-6.9712217,107.6332206,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68e92d13426851:0xa0d241a14441afc9!8m2!3d-6.9712257!4d107.6353807" target="_blank">Pasta Nafisa</h1><br>
+      <h5 class="w3-center">Jl. Babakan Ciamis No.85B, Sukapura, Kec. Dayeuhkolot, Bandung, Jawa Barat 40267</h5>
+    </div>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/6.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+
+    <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://www.google.com/maps/place/Eatalia+Italian+Delights/@-6.9241177,107.6097573,17z/data=!3m1!4b1!4m5!3m4!1s0x2e68e628dd2905f7:0xe178de9808dbb6bb!8m2!3d-6.924123!4d107.611946" target="_blank">Eatalia Italian Delights</h1><br>
+      <h5 class="w3-center">Jl. Lengkong Besar No.45, Paledang, Kec. Lengkong, Kota Bandung, Jawa Barat 40251</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/7.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://gofood.co.id/english/bandung/restaurant/elrey-pasta-e079ef94-1121-4bd3-bbad-d2cf8a805170" target="_blank">Elrey Pasta</h1><br>
+      <h5 class="w3-center">Jl. Panaitan No.23, Kb. Pisang, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40112</h5></a>
+    </div>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/8.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://food.grab.com/id/en/restaurant/warung-pasta-rawamangun-delivery/IDGFSTI000001sx" target="_blank">WARUNG PASTA Rawamangun</h1><br>
+      <h5 class="w3-center">Jl. Balai Pustaka Timur No.53, RT.5/RW.10, Rawamangun, Kec. Pulo Gadung, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13220</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/9.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://gofood.co.id/english/bandung/restaurant/pasta-la-vista-gandapura-17182d71-8ea5-4429-9b9e-3093f63b67e9" target="_blank">Pasta La Vista</h1><br>
+      <h5 class="w3-center">Jl. Gandapura No.73, Merdeka, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40113</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/10.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://food.grab.com/id/en/restaurant/toscana-italian-restaurant-kemang-delivery/6-CYT2V3AXHA6EEE" target="_blank">Toscana</h1><br>
+      <h5 class="w3-center">Jl. Kemang Raya No.120, RT.3/RW.2, Bangka, Kec. Mampang Prpt., Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12730</h5>
+    </div></a>
+  </div></td></tr>
+
+</table>
+  
+
+<table>
+  
+  <caption><h1>Italy</h1></caption>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/11.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://dalmoros.ca/" target="_blank">Dal Moro's</h1><br>
+      <h5 class="w3-center">Calle Casseleria, 5324, 30122 Venezia VE, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/12.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://pastachef.com/" target="_blank">al42 by Pasta Chef Monti</h1><br>
+      <h5 class="w3-center">Via Baccina, 42, 00184 Roma RM, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/Osteria.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://https://osteriafrancescana.it//" target="_blank">Osteria Francescana</h1><br>
+      <h5 class="w3-center">Via Stella, 22, 41121 Modena MO, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/13.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://www.freshpasta2go.it/en/" target="_blank">We Love Italy fresh pasta to go</h1><br>
+      <h5 class="w3-center">Calle de l'Orso, 5529, 30124 Venezia VE, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/Cantina.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://cantinaecucina.it/" target="_blank">Cantina e Cucina</h1><br>
+      <h5 class="w3-center">Via del Governo Vecchio, 87, 00186 Roma RM, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/14.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="https://www.google.com/maps/place/Italy+Restaurant+Milano/@45.466195,9.1855455,20.75z/data=!3m1!5s0x4786c6ad3d9df671:0xc98ec540aa7c214b!4m13!1m7!3m6!1s0x4786c6ad3d991a6b:0xa933c5c72320e501!2sVia+S.+Prospero,+4,+20121+Milano+MI,+Italy!3b1!8m2!3d45.4662142!4d9.185737!3m4!1s0x4786c6ad178c7939:0xf7f89a8a46498d7f!8m2!3d45.4661701!4d9.1858569" target="_blank">Restaurant Milano</h1><br>
+      <h5 class="w3-center">Via S. Prospero, 4, 20121 Milano MI, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+  <tr><td><div class="w3-col m6 w3-padding-large w3-hide-small">
+     <img src="image/17.jpg" class="w3-round w3-image w3-opacity-min" width="750" height="750">
+    </div></td>
+  <td><div class="w3-col m6 w3-padding-large">
+      <h1 class="w3-center"><a href="http://www.trattoriaaldina.it/" target="_blank">Cantina e Cucina</h1><br>
+      <h5 class="w3-center">Via del Governo Vecchio, 87, 00186 Roma RM, Italy</h5>
+    </div></a>
+  </div></td></tr>
+
+</table>
+      </body>
+      </html>
