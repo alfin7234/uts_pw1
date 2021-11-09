e621ae42f860bb7a485fddd953677e9c29346378
alfin7234 <60917377+alfin7234@users.noreply.github.com>
2021-11-10 02:20:56 +0700

    Update index.php

--- uts6/index.php
+++ uts6/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 
 <!--
-Nama (NRP)
-- Alfin Liunardi Senjaya (1672034)
-- William Wijaya (2172011)
-- Morel Mychael Christopher Alpha (2172025)
-
+Nama (NRP) 
+    * - William Wijaya (2172011) 
+    * - Alfin Liunardi Senjaya (1672034)
+    * - Morel Mychael Christopher Alpha (2172025)
+	*
 -->
 <html>
 
@@ -32,7 +32,7 @@ Nama (NRP)
   
 <!-- Header with image -->
 <header class="bgimg display-container grayscale-min" id="home">
-	<img src="images/pizza1.jpg" style="width:100%;" >
+	<img src="images/pasta4.jpg" style="width:100%;" >
 	<div class="display-middle center">
 		<span>Pasta</span>
 		<p><a href="#HOME" class="button xxlarge black" style="font-size:25px">Learn More !</a></p>
@@ -49,18 +49,35 @@ Nama (NRP)
 		<a href="javascript:void(0)" onclick="openMenu(event, 'History');">
 			<div class="col s4 tablink padding-large hover-red">History</div>
 		</a>
-		<a href="javascript:void(0)" onclick="openMenu(event, 'Persebaran');">
-			<div class="col s4 tablink padding-large hover-red">Persebaran</div>
+		<a href="javascript:void(0)" onclick="openMenu(event, 'Nutrition Facts');">
+			<div class="col s4 tablink padding-large hover-red">Nutrition Facts</div>
 		</a>
 	</div>
 
-	<div id="Definisi" class="container menu padding-32 white">
+	<div id="Definisi" class="container menu padding-32 white ">
 		<h1>
 			<b>Pasta</b>
 		</h1>
-		<img src="images/pizza1.jpg" style="width:100%;">
-		<p class="text-grey">
-			*Definisi Pasta*
+		<p class="row justify">
+			Pasta (US: /ˈpɑːstə/, UK: /ˈpæstə/; Italian pronunciation: [ˈpasta]) adalah makanan olahan yang digunakan pada masakan Italia, yang biasanya terbuat dari adonan tidak beragi berbahan tepung gandum durum (semolina) yang dicampur dengan air atau telur dan dibentuk menjadi lembaran-lembaran atau bentuk-bentuk yang beragam, yang kemudian dimasak dengan cara direbus atau dipanggang. Di Indonesia, jenis pasta yang populer adalah spageti, makaroni dan lasagna. Namun, sebenarnya ada banyak lagi jenis pasta lainnya, seperti vermiseli, fetucini, fusilli, penne, ravioli dan sebagainya.<hr>
+		</p>	
+		
+		<img src="images/pasta1.jpg" style="width:100%"><hr>
+		
+		<p class="row justify">
+			Dalam bahasa Inggris, pasta adalah sebutan untuk berbagai jenis masakan dengan bahan utama salah satu jenis pasta yang diberi bumbu dan saus. Pasta berasal dari bahasa Italia "pasta alimentare" yang berarti adonan bahan makanan. Dalam arti luas, "pasta" bisa berarti semua adonan seperti adonan roti, pastri atau bolu. Dalam bahasa Italia, pasta yang berarti bahan makanan seperti mi disebut pastasciutta.<hr>
+		</p>
+		<p class="row justify">
+			Pasta dibuat dari tepung terigu semolina yang merupakan hasil gilingan biji gandum durum dicampur telur sehingga sedikit berwarna kuning cerah, dan bila dimasak dengan benar akan menghasilkan tekstur sedikit kenyal. Pasta buatan Amerika sering dibuat dari campuran tepung terigu Farina dan Semolina, sehingga mempunyai tekstur yang lebih lembut untuk dijadikan hidangan seperti kaserol.<hr>
+		</p>
+		
+		<img src="images/pasta1.jpg" style="width:100%; center"><hr>
+		
+		<p class="row justify">
+			Di luar negara asalnya di Italia, pasta biasanya dijual di dalam kemasan dalam bentuk kering. Pasta kering dibuat di pabrik dengan menggunakan mesin ekstrusi yang mendorong keluar adonan pasta melalui lubang-lubang saringan. Bentuk-bentuk pasta yang lain diperoleh dengan menggiling adonan pasta menjadi lembaran yang kemudian dipotong-potong atau dicetak.<hr>
+		</p>
+		<p class="row justify">
+			Pasta segar buatan rumah tangga dan restoran dibentuk dengan tangan beberapa saat sebelum pasta direbus. Pasta segar memerlukan waktu masak yang singkat, namun pasta segar tidak tahan lama disimpan karena memiliki kadar air yang tinggi. Pasta kering bisa tahan lama disimpan hingga 3 tahun atau lebih karena hanya memiliki kandungan air sebanyak 10%. Pasta yang sudah dimasak dan tinggal dipanaskan dengan oven gelombang mikro juga sering dijumpai di bagian makanan beku pasar swalayan.
 		</p>
 		<hr>
 	</div>
@@ -69,61 +86,106 @@ Nama (NRP)
 		<h1>
 			<b>History</b>
 		</h1>
-		<img src="images/pizza1.jpg" style="width:100%;">
-		<p class="text-grey">
-			*History Pasta*
+		<img src="images/pasta3.jpg" style="width:100%;">
+		<p class="row justify">
+			Makanan mirip pasta banyak dijumpai di berbagai tempat di dunia sejak zaman dulu, terutama di wilayah dengan penduduk yang menjadikan gandum dan serealia sebagai makanan utama. Gandum yang sudah digiling sering dimasak sebagai bubur atau dihaluskan menjadi tepung dan dibuat roti. Pasta kemungkinan besar merupakan alternatif dari bubur atau roti, karena pasta merupakan makanan awetan yang dibuat tanpa perlu dimasak.<hr>
+		</p>
+		<p class="row justify">
+			Di Eropa, catatan tertulis paling tua tentang makanan serupa mi ditemukan pada hiasan makam orang Etruscan di Italia Tengah yang berasal dari tahun 400 SM. Di Tiongkok, mi sudah dikenal sejak tahun 2000 SM berdasarkan hasil penggalian di situs Lajia (Tiongkok barat) yang terletak di tepi Sungai Kuning. Di bawah lapisan tanah jenis tanah loess yang tebal di situs penggalian yang sudah rusak akibat gempa bumi dan banjir ditermukan mi berwarna kuning di dalam panci tanah liat yang terbalik. Panjang mi sekitar setengah meter dengan diameter 3 milimeter. Hasil analisis menunjukkan mi dibuat dari tepung biji milet.
 		</p>
 		<hr>
 	</div>
 
-	<div id="Persebaran" class="container menu padding-32 white">
-		<h1>
-			<b>Persebaran</b>
-		</h1>
-		<img src="images/pizza1.jpg" style="width:100%;">
-		<p class="text-grey">
-			*Persebaran Pasta*
-		</p>
+	<div id="Nutrition Facts" class="row justify container menu padding-32 white">
+	<table  class="row justify">  
+		<tr>
+			<td>
+			<h1>
+				<b>Nutrition Facts</b> 
+			</h1>
+			<p>
+				<table>  
+					<tr><td colspan="3" class="row center large"><b>Nutritional value per 100 g (3.5 oz)</b></td></tr>
+					<tr><td><b>Energy</b></td>				<td><b>660 kJ (160 kcal) <b></td></tr>
+					<tr><td><b>Carbohydrates</b></td>		<td><b>30.9 g</b></td></tr>
+					<tr><td>&nbsp; Starch</td>				<td>26.0 g</td></tr>
+					<tr><td>&nbsp; Sugars</td>				<td>0.6 g</td></tr>
+					<tr><td>&nbsp; Dietary fiber</td>		<td>1.8 g</td></tr>
+					<tr><td><b>Fat</b></td>					<td><b>0.9 g</b></td></tr>
+					<tr><td><b>Protein</b></td>				<td><b>5.8 g</b></td></tr>
+					<tr><td><b>Vitamins</b></td>		<td><b>Quantity</b></td>	<td><b>%DV</b></td></tr>
+					<tr><td>&nbsp; Thiamine (B1)</td>		<td>0.02 mg</td>		<td>2%</td></tr>
+					<tr><td>&nbsp; Riboflavin (B2)</td>		<td>0.02 mg</td>		<td>2%</td></tr>
+					<tr><td>&nbsp; Niacin (B3) </td>		<td>0.4 mg</td>			<td>3%</td></tr>
+					
+					<tr><td>&nbsp; Pantothenic acid (B5) &#160; &#160; &#160; 
+														</td><td>0.11 mg</td>		<td>2%</td></tr>
+														
+					<tr><td>&nbsp; Vitamin B6 </td>			<td>0.05 mg</td>		<td>4%</td></tr>
+					<tr><td>&nbsp; Folate (B9) </td>		<td>7 μg</td>			<td>2%</td></tr>
+					<tr><td>&nbsp; Vitamin C </td>			<td>0 mg</td>			<td>0%</td></tr>
+					<tr><td>&nbsp; Vitamin E </td>			<td>0.06 mg</td>		<td>0%</td></tr>
+					<tr><td><b>Mineral</b></td>				<td><b>Quantity</b></td>	<td><b>%DV</b></td></tr>
+					<tr><td>&nbsp; Calcium </td>			<td>7 mg</td>			<td>1%</td></tr>
+					<tr><td>&nbsp; Iron</td>				<td>0.5 mg</td>			<td>4%</td></tr>
+					<tr><td>&nbsp; Magnesium </td>			<td>18 mg</td>			<td>5%</td></tr>
+					<tr><td>&nbsp; Manganese </td>			<td>0.32 mg</td>		<td>15%</td></tr>
+					<tr><td>&nbsp; Phosphorus </td>			<td>58 mg</td>			<td>8%</td></tr>
+					<tr><td>&nbsp; Potassium </td>			<td>44 mg</td>			<td>1%</td></tr>
+					<tr><td>&nbsp; Sodium </td>				<td>1 mg</td>			<td>0%</td></tr>
+					<tr><td>&nbsp; Zinc </td>				<td>0.5 mg</td>			<td>5%</td></tr>
+					<tr><td><b>Other constituents</b></td>	<td><b>Quantity</b></td></tr>
+					<tr><td>&nbsp; Water </td>				<td>62 g</td></tr>
+							
+				</table>
+			</p>
+			</td>
+			<td>
+				<img src="images/pasta8.jpg" class="row right" style="width:70%;">
+			</td></tr>
 		<hr>
+	</table>
 	</div>
 	<br>
 
   </div>
 </div>
 
-<!-- Contact -->
+<!-- Credits & References -->
 <div class="container padding-64 blue-grey grayscale-min xlarge">
   <div class="content">
-    <h1 class="center xlarge" style="margin-bottom:64px">Credits</h1>
+    <h1 class="center xlarge" style="margin-bottom:64px">Credits & References</h1>
     <p>Sumber data yang kami dapatkan : </p>
-    <p>1. </p>
-    <p>2. </p>
-    <p>3. </p>
+	<p>1. (Inggris) <a href="https://www.instagram.com/alfinliunardi/" title="Alfin Liunardi" target="_blank">www.food-info.net:: Pasta shape</a> Pasta dalam berbagai bentuk</p>
+	<p>2. (Inggris) www.lifeinitaly.com:: History of Pasta Diarsipkan 2008-07-23 di Wayback Machine. Sejarah pasta</p>
+	<p>3. (Inggris) www.annamariavolpi.com How to cook dry pasta Cara merebus pasta</p>
+	<p>4. (Inggris) www.cookandsave.com:: Homemade pasta Cara membuat pasta segar di rumah</p>
+	<p>5. (Inggris) isabelmarants-neakers.us:: The Right Way to Cook Pasta Diarsipkan 2018-02-10 di Wayback Machine. Cara benar membuat pasta</p>
   </div>
 </div>
 
 <!-- Footer -->
 <footer class="center black padding-48 large">
-  <p>Made By 1672034 | 2172011 | 2172025</p>
-  <p class="tiny">Insipired By Some Web Template From W3 School</p>
+	 <p>Made By 1672034 | 2172011 | 2172034</p>
+	<p class="tiny">Insipired By Some Web Template From W3 School</p>
 </footer>
 
 <script>
 // Tabbed Menu
-function openMenu(evt, menuName) {
-  var i, x, tablinks;
-  x = document.getElementsByClassName("menu");
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablink");
-  for (i = 0; i < x.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" red", "");
-  }
-  document.getElementById(menuName).style.display = "block";
-  evt.currentTarget.firstElementChild.className += " red";
-}
-document.getElementById("myLink").click();
+	function openMenu(evt, menuName) {
+		var i, x, tablinks;
+		x = document.getElementsByClassName("menu");
+		for (i = 0; i < x.length; i++) {
+			x[i].style.display = "none";
+		}
+		tablinks = document.getElementsByClassName("tablink");
+		for (i = 0; i < x.length; i++) {
+			tablinks[i].className = tablinks[i].className.replace(" grey", "");
+		}
+		document.getElementById(menuName).style.display = "block";
+		evt.currentTarget.firstElementChild.className += " grey";
+	}
+	document.getElementById("myLink").click();
 </script>
 
 </body>
